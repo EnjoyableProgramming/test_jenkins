dc0c4adb956756dbfbdd2b17833e896828e71075
EnjoyableProgramming <63019353+EnjoyableProgramming@users.noreply.github.com>
2020-07-14 19:55:07 -0500

    dockerfile and app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const { json, urlencoded } = express
+const app = express()
+const port = process.env.PORT || 4300
+app.use(json())
+
+app.use(urlencoded({ extended: false }))
+const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
+
+app.use(cors(corsOptions))
+
+app.use('/', (req, res) => { res.send("this is the API v1.0.0"); })
+
+app.listen(port, () => { console.log(`Server listening on port ${port}`); })
